eec8ddd534622f70fad23e70c72b1635dfee9b1d
Wenbo <lewislwb@foxmail.com>
2018-11-21 11:55:29 +0800

    leetcode2

--- leetcode2.py
+++ leetcode2.py
@@ -1,26 +1,44 @@
-x = -12345
+# Definition for singly-linked list.
+#class ListNode:
+ #   def __init__(self, x):
+  #      self.val = x
+   #     self.next = None
 
-if x > 0:
-    str1 = str(x)
-    str2 = list(str1)
-    str2.reverse()
-    str3 = ''.join(str2)
-    str2 = int(str3)
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        
+        num1=0
+        num2=0
+        summ=0
+        digit=0
+        
+        while l1:
+            num1 = num1 + l1.val * pow(10,digit)
+            l1=l1.next
+            digit+=1
+        
+        
+        digit=0
+        while l2:
+            num2 = num2 + l2.val * pow(10,digit)
+            l2=l2.next
+            digit+=1
+        
+        
+        add = num1 + num2
+        add = str(add)
+        
+        
+       
+        head = ListNode(add[len(add)-1])
+        answer = head
+        for i in reversed(range(0, len(add)-1)):
+            node = ListNode(add[i])
+            head.next = node
+            head = head.next
+        return answer
 
-    if str2 > 2147483648:
-        print(0)
-    else:
-        print(str2)
-else:
-    x = -x
-    str1 = str(x)
-    str2 = list(str1)
-    str2.reverse()
-    str3 = ''.join(str2)
-    str2 = int(str3)
-    str2 = -str2
-    if str2 < -2147483649:
-        print(0)
-    else:
-        print(str2)
-    
